ea0346d889aed842f739183ffdc919b3c7714051
Rene Funcion <rcfun959@gmail.com>
2023-05-06 22:59:23 -0700

    Added scrolling text

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     console.log('JavaScript loaded');
+//     const element = document.getElementById("myElement");
+//     let position = 0;
+//     setInterval(function () {
+//         position += 3;
+//         element.style.left = position + "px";
+//     }, 5);
+// });
+//-----------------
+// document.addEventListener('DOMContentLoaded', () => {
+//     console.log('JavaScript loaded');
+//     const element = document.getElementById("myElement");
+//     let position = 0;
+//     const interval = setInterval(function () {
+//         position += 1;
+//         element.style.left = position + "px";
+//         if (position >= window.innerWidth - element.offsetWidth) {
+//             clearInterval(interval);
+//         }
+//     }, 10);
+// });
 document.addEventListener('DOMContentLoaded', () => {
     console.log('JavaScript loaded');
+    const element = document.getElementById("myElement");
+    let position = window.innerWidth - element.offsetWidth;
+    const interval = setInterval(function () {
+        position -= 1;
+        element.style.left = position + "px";
+        if (position <= 0) {
+            clearInterval(interval);
+        }
+    }, 5);
 });
+
