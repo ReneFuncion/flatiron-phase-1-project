09a1ae6a43370cf9eaedefc4d84c6326bdd468eb
Rene Funcion <rcfun959@gmail.com>
2023-05-06 23:45:15 -0700

    Trying to position text upper right corner

--- script.js
+++ script.js
@@ -20,16 +20,23 @@
 //         }
 //     }, 10);
 // });
+// document.addEventListener('DOMContentLoaded', () => {
+//     console.log('JavaScript loaded');
+//     const element = document.getElementById("myElement");
+//     let position = window.innerWidth - element.offsetWidth;
+//     const interval = setInterval(function () {
+//         position -= 1;
+//         element.style.left = position + "px";
+//         if (position <= 0) {
+//             clearInterval(interval);
+//         }
+//     }, 5);
+// });
 document.addEventListener('DOMContentLoaded', () => {
     console.log('JavaScript loaded');
     const element = document.getElementById("myElement");
-    let position = window.innerWidth - element.offsetWidth;
-    const interval = setInterval(function () {
-        position -= 1;
-        element.style.left = position + "px";
-        if (position <= 0) {
-            clearInterval(interval);
-        }
-    }, 5);
+
+    element.style.left = window.innerWidth - element.offsetWidth;
+
 });
 
