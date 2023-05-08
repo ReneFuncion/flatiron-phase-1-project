df08a63b613bd23cf7943ff53842205bfeae2cf8
Rene Funcion <rcfun959@gmail.com>
2023-05-07 22:23:11 -0700

    Fixed bug where text wraps when separated with space

--- script.js
+++ script.js
@@ -1,50 +1,13 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     console.log('JavaScript loaded');
-//     const element = document.getElementById("myElement");
-//     let position = 0;
-//     setInterval(function () {
-//         position += 3;
-//         element.style.left = position + "px";
-//     }, 5);
-// });
-//-----------------
-// document.addEventListener('DOMContentLoaded', () => {
-//     console.log('JavaScript loaded');
-//     const element = document.getElementById("myElement");
-//     let position = 0;
-//     const interval = setInterval(function () {
-//         position += 1;
-//         element.style.left = position + "px";
-//         if (position >= window.innerWidth - element.offsetWidth) {
-//             clearInterval(interval);
-//         }
-//     }, 10);
-// });
-// document.addEventListener('DOMContentLoaded', () => {
-//     console.log('JavaScript loaded');
-//     const element = document.getElementById("myElement");
-//     let position = window.innerWidth - element.offsetWidth;
-//     const interval = setInterval(function () {
-//         position -= 1;
-//         element.style.left = position + "px";
-//         if (position <= 0) {
-//             clearInterval(interval);
-//         }
-//     }, 5);
-// });
 
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('JavaScript loaded');
-    const element = document.getElementById("myElement");
-    let position = window.innerWidth;
-    const interval = setInterval(function () {
-        position -= 1;
-        element.style.left = position + "px";
-        if (position <= -element.offsetWidth) {
-            element.style.display = "none";
-        }
-        if (position <= -element.offsetWidth - 10) {
-            clearInterval(interval);
+    const myElement = document.getElementById('myElement');
+    let leftPosition = window.innerWidth;
+
+    setInterval(() => {
+        leftPosition--;
+        myElement.style.left = `${leftPosition}px`;
+        if (leftPosition <= -myElement.offsetWidth) {
+            leftPosition = window.innerWidth;
         }
     }, 10);
 });
