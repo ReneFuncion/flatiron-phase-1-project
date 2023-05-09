6067ca80be865cdd4aada2243ec555c24e044e7f
Rene Funcion <rcfun959@gmail.com>
2023-05-08 22:40:46 -0700

    Enclosed code in functions and added scrolling and scroll speed variables

--- script.js
+++ script.js
@@ -2,22 +2,43 @@
 document.addEventListener('DOMContentLoaded', () => {
     const myElement1 = document.getElementById('myElement1');
     const myElement2 = document.getElementById('myElement2');
-    const boxSeparation = 100;
+    const boxSeparation = 200;
+    const scrollSpeed = 5;
     let leftPosition1 = window.innerWidth;
     let leftPosition2 = window.innerWidth + myElement1.offsetWidth + boxSeparation;
-    setInterval(() => {
+
+    // setInterval(() => {
+    //     leftPosition1--;
+    //     myElement1.style.left = `${leftPosition1}px`;
+    //     if (leftPosition1 <= -myElement1.offsetWidth) {
+    //         leftPosition1 = window.innerWidth;
+    //     }
+    // }, 10);
+
+    function moveElement1() {
         leftPosition1--;
         myElement1.style.left = `${leftPosition1}px`;
         if (leftPosition1 <= -myElement1.offsetWidth) {
             leftPosition1 = window.innerWidth;
         }
-    }, 10);
-
-    setInterval(() => {
+    }
+    function moveElement2() {
         leftPosition2--;
         myElement2.style.left = `${leftPosition2}px`;
         if (leftPosition2 <= -myElement2.offsetWidth) {
             leftPosition2 = window.innerWidth;
         }
-    }, 10);
+    }
+    function scrollElements() {
+        setInterval(moveElement1, scrollSpeed);
+        setInterval(moveElement2, scrollSpeed);
+    }
+    scrollElements();
+    // setInterval(() => {
+    //     leftPosition2--;
+    //     myElement2.style.left = `${leftPosition2}px`;
+    //     if (leftPosition2 <= -myElement2.offsetWidth) {
+    //         leftPosition2 = window.innerWidth;
+    //     }
+    // }, 10);
 });
