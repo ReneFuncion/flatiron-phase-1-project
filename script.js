695301f45918e8fad58c15c163908bcf20cc36cd
Rene Funcion <rcfun959@gmail.com>
2023-05-08 22:12:50 -0700

    Added code to scroll second ticker box

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 
 document.addEventListener('DOMContentLoaded', () => {
-    const myElement = document.getElementById('myElement');
-    let leftPosition = window.innerWidth;
+    const myElement1 = document.getElementById('myElement1');
+    const myElement2 = document.getElementById('myElement2');
+    let leftPosition1 = window.innerWidth;
+    let leftPosition2 = window.innerWidth + myElement1.offsetWidth;
+    setInterval(() => {
+        leftPosition1--;
+        myElement1.style.left = `${leftPosition1}px`;
+        if (leftPosition1 <= -myElement1.offsetWidth) {
+            leftPosition1 = window.innerWidth;
+        }
+    }, 10);
 
     setInterval(() => {
-        leftPosition--;
-        myElement.style.left = `${leftPosition}px`;
-        if (leftPosition <= -myElement.offsetWidth) {
-            leftPosition = window.innerWidth;
+        leftPosition2--;
+        myElement2.style.left = `${leftPosition2}px`;
+        if (leftPosition2 <= -myElement2.offsetWidth) {
+            leftPosition2 = window.innerWidth;
         }
     }, 10);
 });
