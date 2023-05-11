3eda87127e45cbbb2ab4e15c47213c32e3b2f7eb
Rene Funcion <rcfun959@gmail.com>
2023-05-11 01:54:14 -0700

    Added code to post current prices inside moving ticker boxes.

--- script.js
+++ script.js
@@ -4,24 +4,20 @@ document.addEventListener('DOMContentLoaded', () => {
     fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
         .then((resp) => resp.json())
         .then((data) => {
-            console.log(data["bpi"]["GBP"]["rate"]);
-            console.log(data["bpi"]["EUR"]["rate"]);
+            postCurrentPrices(data);
         });
 
+    function postCurrentPrices(prices) {
+        document.getElementById('myElement1').textContent = prices["bpi"]["USD"]["rate_float"];
+        document.getElementById('myElement2').textContent = prices["bpi"]["GBP"]["rate_float"];
+    }
     const myElement1 = document.getElementById('myElement1');
     const myElement2 = document.getElementById('myElement2');
+
     const boxSeparation = 200;
-    const scrollSpeed = 5;
+    const scrollSpeed = 10;
     let leftPosition1 = window.innerWidth;
     let leftPosition2 = window.innerWidth + myElement1.offsetWidth + boxSeparation;
-
-    // setInterval(() => {
-    //     leftPosition1--;
-    //     myElement1.style.left = `${leftPosition1}px`;
-    //     if (leftPosition1 <= -myElement1.offsetWidth) {
-    //         leftPosition1 = window.innerWidth;
-    //     }
-    // }, 10);
 
     function moveElement1() {
         leftPosition1--;
@@ -42,11 +38,5 @@ document.addEventListener('DOMContentLoaded', () => {
         setInterval(moveElement2, scrollSpeed);
     }
     scrollElements();
-    // setInterval(() => {
-    //     leftPosition2--;
-    //     myElement2.style.left = `${leftPosition2}px`;
-    //     if (leftPosition2 <= -myElement2.offsetWidth) {
-    //         leftPosition2 = window.innerWidth;
-    //     }
-    // }, 10);
+
 });
